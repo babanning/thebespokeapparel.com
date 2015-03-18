375fe58fd103acd0cdebcbb1985234f79dbe6051
Drew Conley <drewconley@gmail.com>
2015-03-17 21:09:23 -0500

    add svgs and work on header

--- header.php
+++ header.php
@@ -13,10 +13,18 @@
 
 
 <div class="site-header">
-    <ul class="wrap site-header-nav">
-        <li><a href="#" data-location="section-id-products">Products</a></li>
-        <li><a href="#" data-location="section-id-whatwedo">What We Do</a></li>
-        <li><a href="#" data-location="section-id-whoweare">Who We Are</a></li>
-        <li><a href="#" data-location="section-id-contactus">Contact us</a></li>
-    </ul>
+    <div class="site-header-logo-flag">
+        <?php include 'svg/bespoke-logo.svg'; ?>
+    </div>
+    
+        <ul class="wrap site-header-nav left">
+            <li><a href="#" data-location="section-id-products">Products</a></li>
+            <li><a href="#" data-location="section-id-whatwedo">What We Do</a></li>
+            <li><a href="#" data-location="section-id-whoweare">Who We Are</a></li>
+            <li><a href="#" data-location="section-id-contactus">Contact us</a></li>
+        </ul>
+        <ul class="site-header-nav callout-absolute-right">
+            <li><a href="#" data-location="section-id-contactus">Make an appointment</a></li>
+        </ul>
+    
 </div>
